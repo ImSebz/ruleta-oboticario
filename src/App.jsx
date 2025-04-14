b1060e43ce27b9a7bfa9469d222d323ad6ae4502
Sebastián Camargo <juansecapri@hotmail.com>
2025-04-14 16:21:06 -0500

    Avances ruleta

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import './App.css';
 import { useState } from 'react';
 import { Wheel } from 'react-custom-roulette';
 import Modal from 'react-modal';
+import title from './assets/title.png';
+import pointer from './assets/pointer.png';
+import bono50 from './assets/bono_50_mil.png';
+import ramo_de_flores from './assets/ramo_de_flores.png';
+import spa_de_manos from './assets/spa_de_manos.png';
 
 const data = [
-  { option: '1' },
-  { option: '2' },
-  { option: '3' },
-  { option: '4' },
-  { option: '5' },
-  { option: '6' },
-  { option: '7' },
-  { option: '8' },
+  { option: 'bono50', image: { uri: bono50, sizeMultiplier: 1.2 }, alt: "Bono 50 mil" },
+  { option: 'ramo_de_flores', image: { uri: ramo_de_flores, sizeMultiplier: 1.2 }, alt: "Ramo de flores" },
+  { option: 'spa_de_manos', image: { uri: spa_de_manos, sizeMultiplier: 1.2 }, alt: "Spa de manos" },
+  { option: 'bono50', image: { uri: bono50, sizeMultiplier: 1.2 }, alt: "Bono 50 mil" },
+  { option: 'ramo_de_flores', image: { uri: ramo_de_flores, sizeMultiplier: 1.2 }, alt: "Ramo de flores" },
+  { option: 'spa_de_manos', image: { uri: spa_de_manos, sizeMultiplier: 1.2 }, alt: "Spa de manos" },
+  { option: 'bono50', image: { uri: bono50, sizeMultiplier: 1.2 }, alt: "Bono 50 mil" },
+  { option: 'ramo_de_flores', image: { uri: ramo_de_flores, sizeMultiplier: 1.2 }, alt: "Ramo de flores" },
 ];
 
 Modal.setAppElement('#root');
@@ -39,18 +44,26 @@ function App() {
   return (
     <>
       <div className="roulette-container">
+        <div className='title'>
+          <img src={title} alt="Título" />
+        </div>
         <Wheel
           mustStartSpinning={mustSpin}
           prizeNumber={prizeNumber}
           data={data}
-          backgroundColors={['#BBB', '#FF6B00']}
+          backgroundColors={['#f8dbe0', '#d4e6c9', '#6fa487']}
           onStopSpinning={handleStopSpinning}
+          outerBorderWidth={1}
+          outerBorderColor="#fff"
+          radiusLineWidth={0}
+          fontSize={30}
           pointerProps={{
+            src: pointer,
             style: {
               position: 'absolute',
               top: '50%',
               left: '50%',
-              transform: 'translate(-50%, -50%) rotate(-189deg)',
+              transform: 'translate(-50%, -50%) rotate(-145deg)',
               width: '10%',
             },
           }}
@@ -61,12 +74,16 @@ function App() {
         <Modal
           isOpen={isModalOpen}
           onRequestClose={closeModal}
-          contentLabel="Número Generado"
+          contentLabel="Premio Ganado"
           className="modal"
           overlayClassName="overlay"
         >
-          <h2 className='modal-h2'>Número Generado</h2>
-          <p className='modal-number'>{data[prizeNumber].option}</p>
+          <h2 className='modal-h2'>Premio Ganado</h2>
+          <img
+            src={data[prizeNumber].image.uri} // Usa la propiedad `image.uri` para mostrar la imagen
+            alt={data[prizeNumber].alt} // Usa la propiedad `alt` para el texto alternativo
+            style={{ width: '100%', height: 'auto' }} // Ajusta el tamaño de la imagen
+          />
           <button onClick={closeModal}>Cerrar</button>
         </Modal>
       </div>
